c27a9540308ad076a44bcffaf93a741ccdb82877
Rodrick Alcantara <60320390+constROD@users.noreply.github.com>
2020-02-20 11:29:23 +0800

    Updated config/App.js

--- src/config/App.js
+++ src/config/App.js
@@ -4,17 +4,19 @@ dotenv.config()
 const APP_ZONE = process.env.APP_ZONE || 'development'
 const APP_PORT = process.env.APP_PORT || 3000
 
+const API_PROTOCOL = process.env.API_PROTOCOL || 'http'
 const API_HOSTNAME = process.env.API_HOSTNAME || 'localhost'
-const API_PORT = APP_PORT
+const API_PORT = APP_ZONE == 'development' ? (process.env.API_PORT || 3001) : APP_PORT
 
 module.exports = {
   /* APP */
   APP_NAME: process.env.APP_NAME || 'Dev App',
   APP_ZONE,
   APP_PORT,
-  
+
   /* API */
+  API_PROTOCOL,
   API_HOSTNAME,
   API_PORT,
-  API_URL: APP_ZONE == 'production' ? '' : `http://${API_HOSTNAME}:${API_PORT}`
+  API_URL: APP_ZONE == 'production' ? '' : `${API_PROTOCOL}://${API_HOSTNAME}:${API_PORT}`
 }
