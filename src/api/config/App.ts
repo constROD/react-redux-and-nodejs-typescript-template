8fd967828df432e94fbefde68b3d979ca85e4963
Rodrick Alcantara <60320390+constROD@users.noreply.github.com>
2020-02-20 11:37:45 +0800

    Updated config/App.ts

--- src/api/config/App.ts
+++ src/api/config/App.ts
@@ -5,5 +5,8 @@ export default {
   /* APP */
   APP_NAME: process.env.APP_NAME || 'Dev App',
   APP_ZONE: process.env.APP_ZONE || 'development',
-  APP_PORT: process.env.APP_PORT || 3000
+  APP_PORT: process.env.APP_PORT || 3000,
+
+  /* API */
+  API_PORT: process.env.API_PORT || 3001
 }
